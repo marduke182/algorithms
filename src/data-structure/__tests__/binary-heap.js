74c5e68136d045d5841170f8574c50b62e64c271
marduke182 <jrqb182@gmail.com>
2018-09-13 17:37:10 +0200

    feat: implement binary heap

--- src/data-structure/__tests__/binary-heap.js
+++ src/data-structure/__tests__/binary-heap.js
@@ -1,4 +1,4 @@
-import BinaryHeap, { MAX } from '../binary-heap';
+import BinaryHeap, { MAX, MIN } from '../binary-heap';
 
 const identity = x => x;
 
@@ -11,5 +11,44 @@ describe('max heap', () => {
 
   test('should return null, if no element', async () => {
     expect(maxHeap.pop()).toBeNull();
+  });
+
+  test('should return 2, if only exist 2', async () => {
+    maxHeap.push(2);
+
+    expect(maxHeap.pop()).toBe(2);
+  });
+
+  test('should return 5, if push 2 and 5', async () => {
+    maxHeap.push(2);
+    maxHeap.push(5);
+
+    expect(maxHeap.pop()).toBe(5);
+  });
+
+  test('should return 5, if push 5 and 2', async () => {
+    maxHeap.push(2);
+    maxHeap.push(5);
+
+    expect(maxHeap.pop()).toBe(5);
+  });
+
+  test('should pop in order', async () => {
+    [5,7,1,10,20,30,20].forEach(element => maxHeap.push(element));
+
+    [30,20,20,10,7,5,1].forEach(element => expect(maxHeap.pop()).toBe(element))
+  });
+});
+
+describe('min heap', () => {
+  let minHeap;
+
+  beforeEach(async () => {
+    minHeap = new BinaryHeap(identity, MIN);
+  });
+  test('should pop in order', async () => {
+    [5,7,1,10,20,30,20].forEach(element => minHeap.push(element));
+
+    [1,5,7,10,20,20,30].forEach(element => expect(minHeap.pop()).toBe(element))
   });
 });
--- src/data-structure/binary-heap.js
+++ src/data-structure/binary-heap.js
@@ -1,11 +1,16 @@
-export const MAX = (left, right) => Math.max(left, right);
-export const MIN = (left, right) => Math.min(left, right);
+export const MAX = (left, right) => right - left;
+export const MIN = (left, right) => left - right;
 
 export default class BinaryHeap {
   constructor(scoreFunction, compareFunction) {
     this._heap = [];
     this.scoreFunction = scoreFunction;
     this.compareFunction = compareFunction;
+  }
+
+  push(element) {
+    this._heap.push(element);
+    this._bubbleUp(this._heap.length - 1);
   }
 
   pop() {
@@ -23,10 +28,61 @@ export default class BinaryHeap {
     }
 
     return result;
+  }
 
+  _bubbleUp(n) {
+    const element = this._heap[n];
+    const score = this.scoreFunction(element);
+
+    while(n > 0) {
+      const parentN = Math.floor((n + 1) / 2) - 1;
+      const parent = this._heap[parentN];
+
+      if (this.compareFunction(score, this.scoreFunction(parent)) >= 0) {
+        break;
+      }
+
+      this._heap[parentN] = element;
+      this._heap[n] = parent;
+      n = parentN;
+    }
   }
 
   _sinkDown(n) {
+    const length = this._heap.length;
+    const element = this._heap[n];
+    const score = this.scoreFunction(element);
 
+    while (true) {
+      const rightN= (n + 1) * 2;
+      const leftN = rightN - 1;
+
+      let swap = null;
+      let localScore = score;
+      if (leftN < length) { // first child exist
+        const left = this._heap[leftN];
+        const leftScore = this.scoreFunction(left);
+        if(this.compareFunction(score, leftScore) > 0) {
+          swap = leftN;
+          localScore = leftScore;
+        }
+      }
+
+      if(rightN < length) {
+        const right = this._heap[rightN];
+        const rightScore = this.scoreFunction(right);
+        if(this.compareFunction(localScore, rightScore) > 0) {
+          swap = rightN;
+        }
+      }
+
+      if (swap === null) {
+        break;
+      }
+
+      // swap
+      ([this._heap[n], this._heap[swap]] = [this._heap[swap], this._heap[n]]);
+      n = swap;
+    }
   }
 }
